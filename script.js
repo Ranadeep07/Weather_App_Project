323affc9bd736bb0a80c8ef6adca944e41db8b35
Ranadeep <ranadeepbhaduri22@gmail.com>
2020-12-22 18:25:51 +0530

    Changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+const apikey = '3265874a2c77ae4a04bb96236a642d2f';
+
 
 const url = (city) =>
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
